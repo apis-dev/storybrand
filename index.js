31400e0edb0a8e79dc226e02a5fc2a81e9fd388d
Tom Dolen <143192666+tomdolen@users.noreply.github.com>
2023-09-25 13:06:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+export const StoryBrand = {
+  hero: '',
+  problem: {
+    villian: '',
+    internal: '',
+    external: '',
+    philosophical: '',
+  },
+  guide: {
+
+  },
+  plan: {
+
+  },
+  callToAction: {
+
+  },
+  failure: {
+
+  },
+  success: {
+
+  },
+}
